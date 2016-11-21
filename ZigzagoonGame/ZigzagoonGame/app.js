7ca8140db2e47a1009ff8a57cafa056c57893c38
Andy Bhadai <abhadai@gmail.com>
2016-11-21 10:54:08 +0100

    Set up project

---
+++ ZigzagoonGame/ZigzagoonGame/app.js
@@ -0,0 +1,17 @@
+var Game = (function () {
+    function Game() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+    Game.prototype.preload = function () {
+        this.game.load.image('logo', 'zigzagoon.png');
+    };
+    Game.prototype.create = function () {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, ' logo');
+        logo.anchor.setTo(0.5, 0.5);
+    };
+    return Game;
+}());
+window.onload = function () {
+    var game = new Game();
+};
+//# sourceMappingURL=app.js.map
